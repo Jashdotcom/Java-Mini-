ccc2c5cc17684937a457617917a0681e8950c20f
Jash N. Mistry <jashmistry23@gmail.com>
2025-07-28 07:59:09 +0530

    Add files via upload

---
+++ ScopeVar.java
@@ -0,0 +1,13 @@
+public class ScopeVar {
+
+    static int x = 3;
+
+    public static void main(String[] args){
+
+        Int(2);
+        System.out.println(x);
+    }
+    static void Int(int x){
+        System.out.println(x);
+    }
+}
