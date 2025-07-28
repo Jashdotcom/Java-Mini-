e4a4c8e9131696e649035b531fc368aa721bc183
Jash N. Mistry <jashmistry23@gmail.com>
2025-07-28 08:21:13 +0530

    Add files via upload

---
+++ ArraysusingEnhancedFor.java
@@ -0,0 +1,11 @@
+public class ArraysusingEnhancedFor {
+
+    public static void main(String[] args){
+
+        String[] fruits = {"apple","strawberry","orange","banana"};
+
+        for(String fruit : fruits){
+            System.out.println(fruit);
+        }
+    }
+}
