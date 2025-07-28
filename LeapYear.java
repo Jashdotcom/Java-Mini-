e1d082c975e3257b28b37fc98087cc4b12c70576
Jash N. Mistry <jashmistry23@gmail.com>
2025-07-28 08:16:52 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year to know if it is a leap year or not : ");
+        int year = scanner.nextInt();
+
+        if(year%4==0){
+            System.out.printf("%d is a leap year",year);
+        }
+        else{
+            System.out.printf("%d is not a leap year",year);
+        }
+    }
+}
