1a9bdb7811c8051b5cdf28384649f4961d23c57b
Jash N. Mistry <jashmistry23@gmail.com>
2025-07-29 16:18:11 +0530

    Add files via upload

---
+++ SearchingInArray.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SearchingInArray {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int target,size,i;
+
+        System.out.print("Enter the size of array : ");
+        size = scanner.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println();
+
+        for(i=0;i<size;i++){
+            System.out.print("Enter the element : ");
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.print("\nEnter the element to be searched : ");
+        target = scanner.nextInt();
+
+        for(i=0;i<arr.length;i++){
+
+            if(target == arr[i]){
+                System.out.printf("\nElement %d found at index %d",target,i);
+                break;
+            }
+        }
+        scanner.close();
+    }
+}
